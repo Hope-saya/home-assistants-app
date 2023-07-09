cc94d180bdc397670871e506d795517e07f422a1
Nardo Steve <jackcale50@gmail.com>
2023-07-09 21:01:01 +0300

    Updated Migration tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,17 +11,28 @@ return new class extends Migration
      */
     public function up(): void
     {
-
-
+        Schema::create('roles', function(Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
+        });
 
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phoneNumber')->nullable();
+            $table->string('physical_location')->nullable();
+            $table->string('bio')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->unsignedBigInteger('roleId');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            //Foreign Key
+            $table->foreign('roleId')->references('id')->on('roles');
         });
     }
 
---
+++ database/migrations/2023_07_08_092204_create_app_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_postings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('status');
+            $table->string('description');
+            $table->string('salary_range');
+
+             //Foreign keys
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+        Schema::create('job_applications', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('job_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('endDate');
+            $table->string('documents');
+            $table->text('description');
+            $table->timestamps();
+
+            //Foreign keys
+            $table->foreign('job_id')->references('id')->on('job_postings');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('message');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('app');
+    }
+};
